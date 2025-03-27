d85c8caa2ff8277074aa2e169ca17421f28c6a07
junior1star <32687447+junior1star@users.noreply.github.com>
2025-03-27 05:01:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,11 @@ function playSound(word) {
     let audio = document.getElementById(word + "-audio");
     audio.play();
 }
+function addSilentE() {
+    let silentE = document.getElementById("silent-e");
+    let word = document.getElementById("base-word");
+    
+    word.innerText = "Hope";
+    silentE.classList.add("added");
+}
 
